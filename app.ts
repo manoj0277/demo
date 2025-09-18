165d2f807894977d73d892663ec23ec4cdf1a4f4
manoj0277 <jagganimanoj1@gmail.com>
2025-09-18 12:04:05 +0530

    react

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ function addNumbers(a: number, b: number): number {
   return a + b;
 }
 
-let sum: number = addNumbers(5, 10);
+let sum: number = addNumbers(5, 15);
 console.log(`Sum: ${sum}`);
 
 function greet(name: string): void {
